fa81f348933addf1908aed7e063cf0979f275a2b
bulusoni <aditianu1998@gmail.com>
2024-03-29 11:57:29 +0530

    Updating code for Linear Search

--- intermediate.py
+++ intermediate.py
@@ -2,8 +2,8 @@
 
 ######## Approach 1
 n = int(input())
-# c = len(str(n))
-# print(c)
+c = len(str(n))
+print(c)
 
 ######## Approach 2
 import math
@@ -191,7 +191,28 @@ print(maxElements, minElements)
 
 ########################## Linear search using Recursion
 
+def LSearch(x, i, arr):
+    print("1. ", x, i, arr)
+    if i == len(arr) or x < arr[i]:
+        print("B")
+        return 0
+    if x == arr[i]:
+        print("A")
+        return 1
+    else:
+        print("2. ", x, i+1, arr)
+        LSearch(x, i+1, arr)
 
+arr = list(map(int, input().split()))
+print("Element To Search: ", arr[0])
+x = arr[0]
+del arr[0]
+print("List: ", sorted(arr))
+print(LSearch(x, 0, sorted(arr)))
+if LSearch(x, i, sorted(arr)):
+    print("Present")
+else:
+    print("Not Present")
 
 ##########################
 
